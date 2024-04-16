1d5bd31b0be8026d14cf1302d20def4529be769d
Parachichi066 <paxtonmagesa10@gmail.com>
2024-04-16 04:45:25 +0300

    added website files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Oasis-of-Love
 Sandra's Website
+Awwww, you actually bothered to read :D
---
+++ js/main.js
@@ -0,0 +1,29 @@
+let menu = document.querySelector("#menu-btn")
+let navbar = document.querySelector(".navbar")
+
+
+menu.onclick = () =>{
+    menu.classList.toggle("fa-times")
+    navbar.classList.toggle("active")
+}
+
+
+window.onscroll = () =>{
+    menu.classList.remove("fa-times")
+    navbar.classList.remove("active")
+}
+
+ const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.box');
+
+hiddenElements.forEach((el) => observer.observe(el));
