74b546992e3143e307042e6e3afca25f3dcd9016
Parachichi066 <paxtonmagesa10@gmail.com>
2024-04-20 04:57:59 +0300

    added animations
    Edited contacts section too

--- js/main.js
+++ js/main.js
@@ -13,7 +13,8 @@ window.onscroll = () =>{
     navbar.classList.remove("active")
 }
 
- const observer = new IntersectionObserver((entries) =>{
+/* move right */
+const observer = new IntersectionObserver((entries) =>{
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
@@ -27,3 +28,51 @@ window.onscroll = () =>{
 const hiddenElements = document.querySelectorAll('.box');
 
 hiddenElements.forEach((el) => observer.observe(el));
+
+/* fall down */
+const observer2 = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show2');
+        } else {
+            entry.target.classList.remove('show2');
+        }
+    });
+});
+
+const hiddenElements2 = document.querySelectorAll('.box2');
+
+hiddenElements2.forEach((el) => observer2.observe(el));
+
+/* enlarge */
+const observer3 = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show3');
+        } else {
+            entry.target.classList.remove('show3');
+        }
+    });
+});
+
+const hiddenElements3 = document.querySelectorAll('.box3');
+
+hiddenElements3.forEach((el) => observer3.observe(el));
+
+/* move left */
+const observer4 = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show4');
+        } else {
+            entry.target.classList.remove('show4');
+        }
+    });
+});
+
+const hiddenElements4 = document.querySelectorAll('.box4');
+
+hiddenElements4.forEach((el) => observer4.observe(el));
